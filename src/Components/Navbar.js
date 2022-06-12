63673bdc328cf11c62210a09ea58a687376afe77
Sanjoy Roy <sanjoy12k@gmail.com>
2022-06-12 19:05:19 +0530

    navbar modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,21 +5,29 @@ import "./Nav.css";
 export default function Navbar_() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Sukanta Chhatrabas</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="right_align" id="responsive-navbar-nav">
-        <Nav className="nav_elements">
-          <Nav.Link>
-            <Link className="link" to={"/About"}>About</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link className="link" to={"/Contact"}>Contact</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link className="link" to={"/"}>Login/Register</Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand href="#home">Sukanta Chhatrabas</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="right_align" id="responsive-navbar-nav">
+          <Nav className="nav_elements">
+            <Nav.Link>
+              <Link className="link" to={"/About"}>
+                About
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="link" to={"/Contact"}>
+                Contact
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="link" to={"/"}>
+                Login/Register
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
