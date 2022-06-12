bffd4a5af16fcd7bc006f1a67df46515139a409a
dipantan <sanju.mahinagar@gmail.com>
2022-06-12 15:59:41 +0530

    initital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useEffect} from "react";
+
+function App() {
+    useEffect(() => {
+        document.title = "Welcome to CGEC Boys Hostel";
+    })
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
