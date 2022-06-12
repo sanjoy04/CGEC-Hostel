24abaf8d81a93bb7a1b367d136d6f7274ce21e7f
Sanjoy Roy <sanjoy12k@gmail.com>
2022-06-12 17:00:05 +0530

    image set

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,15 +1,17 @@
-// import './App.css';
+import '../App.css';
 import { useEffect } from "react";
 
 function Index() {
-    useEffect(() => {
-        document.title = "Welcome to CGEC Boys Hostel";
-    })
-    return (
-        <div className="App">
-            <h1>Index</h1>
-        </div>
-    );
+  useEffect(() => {
+    document.title = "Welcome to CGEC Boys Hostel";
+  });
+  return (
+    <div className="App">
+      <h1>Index</h1>
+      <div className="parallax"></div>
+    </div>
+    
+  );
 }
 
 export default Index;
