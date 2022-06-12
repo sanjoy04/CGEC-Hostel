5a507a3da29091e14cec08d5a85fda485191cf8b
Sanjoy Roy <sanjoy12k@gmail.com>
2022-06-12 19:02:13 +0530

    Navbar mdified(Right aligned)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,25 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './Nav.css'
+import "./Nav.css";
 
 export default function Navbar_() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand><Link to={'index'}>CGEC - HOSTEL</Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link></Nav.Link>
-            <Nav.Link></Nav.Link>
-            <Nav.Link></Nav.Link>
-            <Nav.Link></Nav.Link>
-          </Nav>
-          <Nav className="nav_bar">
-            <Link to={"/About"}>About</Link>
-            <Link to={"/Contact"}>Contact</Link>
-            <Link to={""}>Login</Link>
-            <Link to={""}>signup</Link>
-
-           
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      <Navbar.Brand href="#home">Sukanta Chhatrabas</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse className="right_align" id="responsive-navbar-nav">
+        <Nav className="nav_elements">
+          <Nav.Link>
+            <Link className="link" to={"/About"}>About</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link className="link" to={"/Contact"}>Contact</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link className="link" to={"/"}>Login/Register</Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
