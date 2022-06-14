0e36a184092154bffbea23b8be9b65b7d41c081b
Tinku Mahato <tinkumatho514@gmail.com>
2022-06-14 19:42:08 +0530

    h

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,23 +1,66 @@
 
 import '../App.css';
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import Navbar_ from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import { Carousel } from 'bootstrap';
+
+
 
 function Index() {
   useEffect(() => {
     document.title = "Welcome to CGEC Boys Hostel";
   });
-  return (
-    <div className="App">
-      <Navbar_/>
-      <div className="parallax"></div>
-      <Footer/>
 
-    </div>
-    
+  function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  return (
+    <Carousel>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=373940"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Second slide&bg=282c34"
+        alt="Second slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Third slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  
   );
 
-}
 
+}
+}
 export default Index;
+
