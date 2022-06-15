1d93bd43aad29ec5bbf117b1f4822652c7cbd910
dipantan <sanju.mahinagar@gmail.com>
2022-06-15 18:49:37 +0530

    gdfgdfg

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,41 +3,20 @@ import '../App.css';
 import { useEffect, useState } from "react";
 import Navbar_ from '../Components/Navbar';
 import Footer from '../Components/Footer';
-// import { Carousel } from 'react-responsive-carousel';
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Slider from '../Components/Slider';
 
-
 function Index() {
-
   const [index, setIndex] = useState(0);
-
-
-  // const handleSelect = (selectedIndex, e) => {
-  //   setIndex(selectedIndex);
-  // }
-
-
-
   useEffect(() => {
     document.title = "Welcome to CGEC Boys Hostel";
-
-
   });
 
   return (
 
-    <div>
-      <div>
-        <Navbar_ />
-      </div>
-      <div>
-        <Slider />
-      </div>
-
-      <div>
-        <Footer />
-      </div>
+    <div className="App">
+      <Navbar_ />
+      <Slider />
+      <Footer />
     </div>
 
   );
